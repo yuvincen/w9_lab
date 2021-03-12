1cca33df59615d4d12e7d9ba71ec74583e303102
VIncent Yu <vyu27@my.bcit.ca>
2021-03-12 13:10:29 -0800

    Commiting New File(s)

---
+++ app.js
@@ -0,0 +1,19 @@
+let name = ["Vince", "Billy", "Samantha", "George", "Andrew"];
+
+let num1 = 26
+
+const num2 = 15
+
+const bo1 = true
+
+
+if (num1 === num2) {
+    result = "they match";
+}   else {
+    result = "no luck";
+}
+
+
+for (let name = 0; name < 5; name++) {
+    console.log(name);
+}
